95b076396d4bd8929939ef80984ff8244071ea92
Matt Williams <matt.seongjun@gmail.com>
2019-05-17 12:56:11 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
+    attr_reader :knowledge
+
+    def initialize(knowledge=[])
+        @knowledge = knowledge
+    end
+
+    def learn(info)
+       self.knowledge << info 
+    end
 
 end
